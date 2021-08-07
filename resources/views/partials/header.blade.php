05f8b896408c13df014095f65a244d48a858850c
collinberg <berg.collin@gmail.com>
2021-08-07 14:54:34 -0400

    Added top nav.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,11 @@
   <div class="container-fluid px-4">
     <div class='flex justify-between'>
       <a class="brand font-bold text-2xl uppercase border-none" href="{{ home_url('/') }}"><img src='{{ $logo }}' class='brand-logo h-16 mt-5' alt="{{ $site_name }}"></a>
+      <div class='hidden md:block'>
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav text-right py-2']) !!}
       @endif
+    </div>
       <button id='menuToggle' type='button' class='rounded-full block md:hidden bg-primary w-12 h-12 bg-primary shadow-lg mt-5 relative collapse mr-8' aria-label="Mobile Menu Toggle">
         <span class='sr-only'></span>
         <span class='menu-bar'></span>
