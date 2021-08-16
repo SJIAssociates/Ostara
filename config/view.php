728225424c36e14be89913187ed7021548c1f62d
collinberg <berg.collin@gmail.com>
2021-08-16 15:31:10 -0400

    Moved Cache for WPE

--- config/view.php
+++ config/view.php
@@ -29,8 +29,8 @@ return [
     |
     */
 
-    'compiled' => wp_upload_dir()['basedir'].'/cache',
-
+    //'compiled' => wp_upload_dir()['basedir'].'/cache',
+    'compiled' => '/tmp/sage-cache',
 
     /*
     |--------------------------------------------------------------------------
