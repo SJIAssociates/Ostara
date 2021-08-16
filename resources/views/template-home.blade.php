dd17dcef5ae9241db4b126b44bd9fb11775101e3
collinberg <berg.collin@gmail.com>
2021-08-16 15:33:17 -0400

    Updated hte Homepage typography

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -5,38 +5,44 @@
 @extends('layouts.app')
 
 @section('content')
-<section id='landing' class='home-section' style="background: url( @php echo site_url() . '/wp-content/themes/ostara/dist/images/Background.jpg' @endphp ); background-size: cover; background-position: center center;">
+<section id='landing' class='home-section ' style="background: url( @php echo site_url() . '/wp-content/themes/ostara/dist/images/Background.jpg' @endphp ); background-size: cover; background-position: center center;">
+  <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-1.png' @endphp " id='line-1'>
   <div class='container flex flex-col justify-center'>
     <div class='flex'>
       <div class='w-full md:w-1/2'>
         <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/Ostara-logo.png' @endphp" class='mb-5 mx-auto w-3/4 md:w-full' alt="Ostara Logo">
-        <p class='text-white font-bold text-2xl md:text-4xl text-center'>We offer transformational<br>problem-solving.</p>
+        <p class='text-white text-2xl lg:text-4xl text-center font-medium'>We offer transformational<br>problem-solving.</p>
       </div>
     </div>
   </div>
 </section>
 
 <section class='relative' id='homeInfo'>
+  <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-2.png' @endphp " id='line-2'>
+  <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-3.png' @endphp " id='line-3'>
+
   <div class='container flex flex-col justify-center py-5 lg:py-24'>
-    <div class='w-full' >
-      <div class='w-full md:w-1/2'>
+    <div class='w-full'>
+      <div class='w-full lg:w-1/2'>
         <p class='text-lg lg:text-2xl'>In a world of accelerating challenges, our values-driven boutique advisory firm—borne of 20+ years of dynamic executive leadership—offers strategic, customized solutions for growth and innovation. We have the proven experience and an expansive network to lead you through the critical decisions that will fuel positive change and create real value.</p>
       </div>
     </div>
     <div class='w-full relative lg:py-24 min-h-screen'>
-      <div class='w-full md:w-1/2'>
-        <h2 class='leading-normal md:leading-loose font-bold text-blue-dark text-2xl md:text-4xl lg:mt-32'>We partner exclusively with open-minded, creative, courageous leaders and entrepreneurs.</h2>
+      <div class='w-full lg:w-1/2'>
+        <h2 class='leading-normal text-blue-dark text-2xl lg:text-4xl lg:mt-32'>We partner exclusively with open-minded, creative, courageous leaders and entrepreneurs.</h2>
       </div>
     </div>
   </div>
   <div class='home-circle'>
-    <p>These forward-thinking, confident leaders are committed to making the often difficult and necessary decisions and investments that create value, while also nurturing enduring cultures to feed and fuel sustainable bottomline growth. Leaders who operate openly and transparently, and act on “what they don’t want to hear” in order to realize their transformative vision.</p>
+    <p class='animate__animated animate__fadeIn'>These forward-thinking, confident leaders are committed to making the often difficult and necessary decisions and investments that create value, while also nurturing enduring cultures to feed and fuel sustainable bottomline growth. Leaders who operate openly and transparently, and act on “what they don’t want to hear” in order to realize their transformative vision.</p>
   </div>
 </section>
 
 <section class='home-section bg-primary text-white' id='Homelist'>
+  <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-4.png' @endphp " id='line-4'>
+
   <div class='container flex flex-col justify-center py-5 lg:py-24'>
-    <h2 class='w-full md:w-3/4 font-bold text-2xl md:text-4xl mb-5 lg:mb-10'>We craft a custom approach to deliver measurable impact where it’s needed most.</h2>
+    <h2 class='w-full md:w-3/4 text-2xl md:text-4xl mb-5 lg:mb-10 font-medium'>We craft a custom approach to deliver measurable impact where it’s needed most.</h2>
     <div class='flex flex-row flex-wrap'>
       <div class='w-full md:w-1/2 md:pr-10'>
         <ul class='text-black'>
