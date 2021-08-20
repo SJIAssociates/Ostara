f414bae9eea31e6554a7ff43171db47ed6ddd434
collinberg <berg.collin@gmail.com>
2021-08-20 14:29:13 -0400

    Updated the Header & Homepage

--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section id='landing' class='home-section ' style="background: url( @php echo site_url() . '/wp-content/themes/ostara/dist/images/Background.jpg' @endphp ); background-size: cover; background-position: center center;">
+<section id='landing' class='' style="background: url( @php echo site_url() . '/wp-content/themes/ostara/dist/images/Background.jpg' @endphp ); background-size: cover; background-position: center center;">
   <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-1.png' @endphp " id='line-1'>
   <div class='container flex flex-col justify-center'>
     <div class='flex'>
@@ -21,15 +21,15 @@
   <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-2.png' @endphp " id='line-2'>
   <img src="@php echo site_url() . '/wp-content/themes/ostara/dist/images/pg1-line-3.png' @endphp " id='line-3'>
 
-  <div class='container flex flex-col justify-center py-5 lg:py-24'>
+  <div class='container flex flex-col justify-center py-5 md:py-24'>
     <div class='w-full'>
-      <div class='w-full lg:w-1/2'>
+      <div class='w-full md:w-3/4 lg:w-1/2'>
         <p class='text-lg lg:text-2xl'>In a world of accelerating challenges, our values-driven boutique advisory firm—borne of 20+ years of dynamic executive leadership—offers strategic, customized solutions for growth and innovation. We have the proven experience and an expansive network to lead you through the critical decisions that will fuel positive change and create real value.</p>
       </div>
     </div>
-    <div class='w-full relative lg:py-24 min-h-screen'>
-      <div class='w-full lg:w-1/2'>
-        <h2 class='leading-normal text-blue-dark text-2xl lg:text-4xl lg:mt-32'>We partner exclusively with open-minded, creative, courageous leaders and entrepreneurs.</h2>
+    <div class='w-full relative lg:py-32 lg:my-10 min-h-screen'>
+      <div class='w-full md:w-1/2'>
+        <h2 class='leading-normal text-blue-dark text-2xl lg:text-4xl md:pt-10 lg:pt-4 md:mt-32'>We partner exclusively with open-minded, creative, courageous leaders and entrepreneurs.</h2>
       </div>
     </div>
   </div>
@@ -44,7 +44,7 @@
   <div class='container flex flex-col justify-center py-5 lg:py-24'>
     <h2 class='w-full md:w-3/4 text-2xl md:text-4xl mb-5 lg:mb-10 font-medium'>We craft a custom approach to deliver measurable impact where it’s needed most.</h2>
     <div class='flex flex-row flex-wrap'>
-      <div class='w-full md:w-1/2 md:pr-10'>
+      <div class='w-full md:w-1/2 lg:w-2/5 md:pr-10'>
         <ul class='text-black'>
         <li>Comprehensive market opportunity analysis</li>
         <li>Ready-to-execute rapid, sustainable growth and revenue strategies</li>
@@ -57,7 +57,7 @@
         <li>Business Model Development</li>
       </ul>
       </div>
-      <div class='w-full md:w-1/3'>
+      <div class='w-full md:w-1/2 lg:w-1/3'>
         <ul class='text-black'>
         <li>Growth and commercialization strategy</li>
         <li>Organization & Culture Design</li>
